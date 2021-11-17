68f811a16b4bb9b9c28b50cd3e6c800c5a28b83c
Han Park <hanpark@Hans-MacBook-Pro.local>
2021-11-17 17:13:18 +0900

    graphs added on userPage

--- my-app/src/components/user/overviewGraph.js
+++ my-app/src/components/user/overviewGraph.js
@@ -3,15 +3,39 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
+import { Line } from "react-chartjs-2";
+
+const data = {
+    labels: ['1', '2', '3', '4', '5', '6'],
+    datasets: [
+      {
+        label: '# of Posts',
+        data: [12, 19, 3, 5, 2, 3],
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+      },
+    ],
+  };
+  
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  };
+
+
 export default function OverviewGraph() {
+
   return (
     <div>
      <Typography variant="h5" gutterBottom component="div" sx={{ml: 1, mt: 3, fontSize: 20}}>
         최근 한달 간 n개의 포스트를 남겼습니다.
       </Typography> 
-      <Paper variant="outlined" sx={{ p: 20}}>
-      (그래프 들어갈 자리)
-
+      <Paper variant="outlined" sx={{ p: 2}}>
+        <Line data={data} options={options} />
       </Paper>
     </div>
   );
--- my-app/src/components/user/overviewTagInfo.js
+++ my-app/src/components/user/overviewTagInfo.js
@@ -3,14 +3,35 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
+import { Radar } from 'react-chartjs-2';
+
+const data = {
+    labels: ['Thing 1', 'Thing 2', 'Thing 3', 'Thing 4', 'Thing 5', 'Thing 6'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [2, 9, 3, 5, 2, 3],
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+  
+  const options = {
+    scale: {
+      ticks: { beginAtZero: true },
+    },
+  };
+
 export default function OverviewTagInfo() {
   return (
     <div>
      <Typography variant="h5" gutterBottom component="div" sx={{ml: 1, mt: 3, fontSize: 20}}>
         최근 한달 간 가장 많이 사용한 태그는 '테크', '예술', '생각' 입니다.
       </Typography> 
-      <Paper variant="outlined" sx={{ p: 20}}>
-      (그래프 들어갈 자리)
+      <Paper variant="outlined" sx={{ p: 2}}>
+      <Radar data={data} options={options} />
 
       </Paper>
     </div>
